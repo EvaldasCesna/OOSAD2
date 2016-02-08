00d80e9e942cba2de34bfedb48e5e6cc38b2d761
Evaldas Cesna <x00112310@gmail.com>
2016-02-08 20:01:54 +0000

    PokerGame

---
+++ PokerGame/Card.java
@@ -0,0 +1,21 @@
+public class Card {
+
+	private int rank;
+	private int suit;
+	private boolean faceUp;
+
+	/**
+	 * 
+	 * @param Card
+	 */
+	public int compareTo(int Card) {
+		// TODO - implement Card.compareTo
+		throw new UnsupportedOperationException();
+	}
+
+	public void flip() {
+		// TODO - implement Card.flip
+		throw new UnsupportedOperationException();
+	}
+
+}
