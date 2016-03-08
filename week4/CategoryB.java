62be92333928adde02c375e6147e5fffd04c9e28
Evaldas Cesna <x00112310@gmail.com>
2016-03-08 15:44:33 +0000

    Labwork

---
+++ week4/CategoryB.java
@@ -0,0 +1,8 @@
+public class CategoryB implements SalaryCalculator {
+
+	public double operation() {
+		// TODO - implement CategoryB.operation
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ week4/Employee.java
@@ -0,0 +1,10 @@
+public class Employee {
+
+	private String name;
+
+	public void display() {
+		// TODO - implement Employee.display
+		throw new UnsupportedOperationException();
+	}
+
+}
